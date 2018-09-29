20b5d84c42415aa76e5f6e75c382ac0c5aa58bb6
Hosta <hosta.plantain@gmail.com>
2018-09-29 23:11:08 +0900

    Update Tilex2CompressedEssentiaJars.java

--- src/main/java/sol3675/middleearththaumaturgy/common/tileentities/Tilex2CompressedEssentiaJars.java
+++ src/main/java/sol3675/middleearththaumaturgy/common/tileentities/Tilex2CompressedEssentiaJars.java
@@ -16,16 +16,16 @@ public class Tilex2CompressedEssentiaJars extends TileJarFillable implements IAs
 	
 	@Override
 	public int getMinimumSuction() {
-	  return this.aspectFilter != null ? 56 + (amount * 64 / this.max) : 48 + (amount * 64 / this.max);
+	  return this.aspectFilter != null ? 56 + (amount * 8 / this.max) : 48 + (amount * 8 / this.max);
 	}
 
 	@Override
 	public int getSuctionAmount(ForgeDirection loc) {
 		if (this.amount < this.maxAmount){
 			if (this.aspectFilter != null) {
-				return 56 + (amount * 64 / this.max);
+				return 56 + (amount * 8 / this.max);
 			}
-			return 48 + (amount * 64 / this.max);
+			return 48 + (amount * 8 / this.max);
 		}
 		return 0;
 	}
